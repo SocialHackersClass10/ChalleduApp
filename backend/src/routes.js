f01be979f2a92166e1df4b7c312e442a7613f96e
Stefanos Leventis <sigianas@yahoo.com>
2020-06-09 23:28:14 +0300

    changed route to 'users', changed branch name

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.get('/users', (req, res) => {
     res.send(`<h1>No users yet :(</h1>`)
 })
 
-router.post('/user-management/users', (req, res) => {
+router.post('/users', (req, res) => {
     res.status(200).send(req.body)
 })
 
