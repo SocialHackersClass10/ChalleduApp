71b971fd94629bdad5fe837f47a87754e060073e
DOT.NOT <dotnot@UFD.016-MINT.192>
2020-06-12 15:02:11 +0300

    1st commit: partial implementation to overcome bottleneck

--- backend/src/Schema.js
+++ backend/src/Schema.js
@@ -1,4 +1,12 @@
+
 const mongoose = require("mongoose");
+
+// mongodb: (may be) needed for targeted updates
+mongoose.set('useFindAndModify',false);
+
+// export enables all modules to use same connection
+// connection initialized at "main" as defined in package.json
+module.exports=mongoose;
 
 const affinitiesArray = [
     "ΜΗΔΕΝΙΚΗ ΦΤΩΧΕΙΑ", "ΜΗΔΕΝΙΚΗ ΠΕΙΝΑ",
@@ -11,18 +19,20 @@ const affinitiesArray = [
     "ΕΙΡΗΝΗ, ΔΙΚΑΙΟΣΥΝΗ ΚΑΙ ΙΣΧΥΡΟΙ ΘΕΣΜΟΙ", "ΣΥΝΕΡΓΑΣΙΑ ΓΙΑ ΤΟΥΣ ΣΤΟΧΟΥΣ"
 ];
 
-const usersSchema = mongoose.Schema({ 
+const usersSchema = mongoose.Schema({
     approval_pending: Boolean,
     username: {type:String, trim: true},
     password: {type:String, trim: true},
-    email: {type:String, trim: true}, 
+    email: {type:String, trim: true},
     full_name: {type:String, trim: true},
-    role: { type: String, trim: true, enum: ['NGO', 'administrator', 'User'] },
-    affiliated_org: {type:String, trim: true},  //the NGO or NGOs that mentor may belongs
-    organization_id: {type: ObjectId, default:""}, //the _id of the NGO or NGOs that mentor may belongs
-    affinities: [{type:String, enum: affinitiesArray}], //an array of _id affinities
+    role: { type: String, trim: true, enum: ['administrator', 'User'] },
+    affiliated_ngo: {       // data specific to the NGO this user is affiliated with
+        ID: {type: String, default:""},     // equals NGO._id
+        name: {type:String, trim: true},    // NGO.name
+    },
+    affinities: [{type:String, enum: affinitiesArray}], //an array of affinities
     title: {type:String, trim: true},  //user's professional title
-    gender: {type: {type:String, trim: true}, enum: ['other', 'male', 'female']},
+    gender: {type: {type:String, trim: true}, enum: ['female', 'male', 'other']},
     image: {type:String, trim: true},
     webpage: {type:String, trim: true},
     birth_date: Date,
@@ -30,9 +40,9 @@ const usersSchema = mongoose.Schema({
     contact: {
         address: [{type:String, trim: true}],
         phone: [{type:String, trim: true}],
-        contact_hours: [{type:String, trim: true}] 
+        contact_hours: [{type:String, trim: true}]
     }
-}); 
+});
 
 
 const NGOsSchema = mongoose.Schema({
@@ -46,7 +56,7 @@ const NGOsSchema = mongoose.Schema({
     contact: {
         address: [{type:String, trim: true}],
         phone: [{type:String, trim: true}],
-        contact_hours: [{type:String, trim: true}] 
+        contact_hours: [{type:String, trim: true}]
     },
     documents: [Buffer]
 });
@@ -54,3 +64,4 @@ const NGOsSchema = mongoose.Schema({
 
 const Users = mongoose.model('Users', usersSchema);
 const NGOs = mongoose.model('NGOs', NGOsSchema);
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,13 +1,49 @@
+
+// dotenv: access local environmental constants / variables
+require('dotenv').config();
+
 const express = require('express');
 const path = require('path')
 const app = express();
 
-//Middleware
+// import mongoose from our Schema
+const mongoose = require("./Schema");
+
+// Middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'src')))
 app.use('/', require('./routes'))
 
-//Port
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Initialized at ${PORT}`))
+// dotenv: SERVER_PORT
+const PORT = process.env.SERVER_PORT || 5000;
+
+// mongodb: db connection params
+const mongoConxParams = {useNewUrlParser:true, useUnifiedTopology:true};
+
+// server: initial greeting
+console.log('\nServer initialization ...\n');
+
+// dotenv: verify existing conncetion URL and credentials / terminate if missing
+if (!process.env.MONGODB_KEY) {
+    terminateServer('Missing database connection URL');
+};
+
+// mongodb: initialise database conncetion / terminate on error
+mongoose.connect(process.env.MONGODB_KEY, mongoConxParams, err => {
+    if (err) {
+        const errorMsg = 'Database connection Error:\n'
+                        +(err.code   ?`Code: ${err.code}`+'\n':'')
+                        +(err.message?err.message+'\n':'');
+        terminateServer(errorMsg);
+        process.exit(1);
+    };
+    app.listen(PORT, () => console.log(`Server ready, listening on port ${PORT}`));
+});
+
+// log a message and stop the server
+function terminateServer(message){
+    console.log(message,'\nThe Server is terminating.\n');
+    process.exit(1);
+};
+
