1c2d313144cb71c5aceb8d47c6d11b9941586f47
ligopsychakis-michalis <ligopsyxakis.m@gmail.com>
2020-06-15 20:12:06 +0300

    get single user

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,6 +5,21 @@ router.get('/users', (req, res) => {
     res.send(`<h1>No users yet :(</h1>`)
 })
 
+//route to get a single user
+router.get('/users/:id', async (req,res) => {
+    try{
+        let user = await Users.findById(req.params.id);
+        if (user){
+            delete user.password;
+            res.status(200).json(user);
+        }else{
+            res.status(404).send({ error: `The given id ${req.params.id} not found.` });
+        };
+    }catch(err){
+        res.status(404).send({ error: err });
+    };    
+});
+
 router.post('/users', (req, res) => {
     res.status(200).send(req.body)
 })
