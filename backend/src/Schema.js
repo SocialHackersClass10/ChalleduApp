606be1d39f4e6049b5bdf1b6bc4bf9145414fe2b
Stefanos Leventis <sigianas@yahoo.com>
2020-06-17 00:04:52 +0300

    Finished with the get all users route

--- backend/src/Schema.js
+++ backend/src/Schema.js
@@ -2,11 +2,11 @@
 const mongoose = require("mongoose");
 
 // mongodb: (may be) needed for targeted updates
-mongoose.set('useFindAndModify',false);
+mongoose.set('useFindAndModify', false);
 
 // export enables all modules to use same connection
 // connection initialized at "main" as defined in package.json
-module.exports.mongoose=mongoose;
+module.exports.mongoose = mongoose;
 
 const affinitiesArray = [
     "ΜΗΔΕΝΙΚΗ ΦΤΩΧΕΙΑ", "ΜΗΔΕΝΙΚΗ ΠΕΙΝΑ",
@@ -22,42 +22,42 @@ const affinitiesArray = [
 // keep schema and model in singular
 const userSchema = mongoose.Schema({
     approval_pending: Boolean,
-    username: {type:String, trim: true},
-    password: {type:String, trim: true},
-    email: {type:String, trim: true},
-    full_name: {type:String, trim: true},
+    username: { type: String, trim: true },
+    password: { type: String, trim: true },
+    email: { type: String, trim: true },
+    full_name: { type: String, trim: true },
     role: { type: String, trim: true, enum: ['administrator', 'User'] },
     affiliated_ngo: {       // data specific to the NGO this user is affiliated with
-        ID: {type: String, default:""},     // equals NGO._id
-        name: {type:String, trim: true},    // NGO.name
+        ID: { type: String, default: "" },     // equals NGO._id
+        name: { type: String, trim: true },    // NGO.name
     },
-    affinities: [{type:String, enum: affinitiesArray}], //an array of affinities
-    title: {type:String, trim: true},  //user's professional title
-    gender: {type: {type:String, trim: true}, enum: ['female', 'male', 'other']},
-    image: {type:String, trim: true},
-    webpage: {type:String, trim: true},
+    affinities: [{ type: String, enum: affinitiesArray }], //an array of affinities
+    title: { type: String, trim: true },  //user's professional title
+    gender: { type: { type: String, trim: true }, enum: ['female', 'male', 'other'] },
+    image: { type: String, trim: true },
+    webpage: { type: String, trim: true },
     birth_date: Date,
-    description: {type:String, trim: true},
+    description: { type: String, trim: true },
     contact: {
-        address: [{type:String, trim: true}],
-        phone: [{type:String, trim: true}],
-        contact_hours: [{type:String, trim: true}]
+        address: [{ type: String, trim: true }],
+        phone: [{ type: String, trim: true }],
+        contact_hours: [{ type: String, trim: true }]
     }
 });
 
 // keep schema and model in singular
 const NGOSchema = mongoose.Schema({
     approval_pending: Boolean,
-    name: {type:String, trim: true},
-    image: {type:String, trim: true},
-    webpage: {type:String, trim: true},
-    description: {type:String, trim: true},
-    main_representative: {type:String, trim: true},
-    affinities: [{type:String, enum: affinitiesArray}], //an array of _id affinities
+    name: { type: String, trim: true },
+    image: { type: String, trim: true },
+    webpage: { type: String, trim: true },
+    description: { type: String, trim: true },
+    main_representative: { type: String, trim: true },
+    affinities: [{ type: String, enum: affinitiesArray }], //an array of _id affinities
     contact: {
-        address: [{type:String, trim: true}],
-        phone: [{type:String, trim: true}],
-        contact_hours: [{type:String, trim: true}]
+        address: [{ type: String, trim: true }],
+        phone: [{ type: String, trim: true }],
+        contact_hours: [{ type: String, trim: true }]
     },
     documents: [Buffer]
 });
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,26 +1,31 @@
 const express = require('express');
-const {User} = require("./Schema");
+const { User } = require("./Schema");
 const router = express.Router();
 
-
-router.get('/users', (req, res) => {
-    res.send(`<h1>No users yet :(</h1>`)
+//Route to get all users
+router.get('/users', async (req, res) => {
+    const users = await User.find({});
+    try {
+        res.status(200).send(users)
+    } catch (err) {
+        res.status(500).send(err)
+    }
 });
 
 
 //route to get a single user
-router.get('/users/:id', async (req,res) => {
-    try{
+router.get('/users/:id', async (req, res) => {
+    try {
         let user = await User.findById(req.params.id);
-        if (user){
+        if (user) {
             delete user.password;
             res.status(200).json({ user: user });
-        }else{
+        } else {
             res.status(404).send({ error: `User with id ${req.params.id} not found.` });
         };
-    }catch(err){
+    } catch (err) {
         res.status(500).send({ error: err });
-    };    
+    };
 });
 
 
