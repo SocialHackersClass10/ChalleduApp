dfb9515ba647639bc8365fb9005b25ebd6e05e29
bbouba <56887079+bbouba@users.noreply.github.com>
2020-06-29 21:14:51 +0300

    Jwt Middleware

---
+++ backend/src/middleware/validate-Roles.js
@@ -0,0 +1,10 @@
+module.exports = function HasRole(roles) {
+    return function(req, res, next) {
+        if (!roles.includes(req.user.role)) {
+            res.status(403).json({
+                errror: 'You are unauthorized to access this resource.'
+            });
+        }
+        next();
+    };
+};
