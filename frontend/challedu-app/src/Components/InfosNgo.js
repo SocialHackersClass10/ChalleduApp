b16683426a9271be277a7502f5c87611a26fa2b4
mouzakiskir <56204532+mouzakiskir@users.noreply.github.com>
2020-07-13 09:58:55 +0300

    Adding profile page for users and ngos (#98)

---
+++ frontend/challedu-app/src/Components/InfosNgo.js
@@ -0,0 +1,44 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import NgoProvider from "../NGOProvider";
+
+const DetailsNgo = ({match}) => {
+    const [data, setData]=useState(null)
+    const [checkError, setCheckError]=useState(null)
+    const getData=async ()=>{
+        try{
+            const loginResult = await NgoProvider.getNGO(match.params.id, localStorage.getItem('access_token'));
+            setData(loginResult.ngo)
+        }catch(err){
+            setCheckError(err.message)
+        }
+    }
+    useEffect(()=>{
+       getData()
+    }, [])
+    return (
+        <div>
+            <h1>User Details:</h1>
+            <ul>
+            {
+                data!==null ? 
+                <ul>
+                    <li>Name: {data.name}</li>
+                    <li>Affinities: {data.affinities}</li>
+                    <li>Main Representative: {data.main_representative}</li>
+                    <li>Web Page: {data.webpage}</li>
+                    <li>Picture: {data.image}</li>
+                    <li>Brief description: {data.description}</li>
+                    <li>Adress: {data.contact.address}</li>
+                    <li>Phone: {data.contact.phone}</li>
+                    <li>Contact Hours: {data.contact.contact_hours}</li>
+                    <li>Documents: Will be upload later.....................</li>
+                </ul>
+                : <p>{checkError}</p>  
+            }
+            </ul>
+        </div>
+    )
+}
+
+export default DetailsNgo;
