68316c1841936448d297215ffc3929963fb2d954
ligopsychakis-michalis <ligopsyxakis.m@gmail.com>
2020-07-30 20:55:39 +0300

    fix userContex refresh bug

--- frontend/challedu-app/src/components/RouteForAll.js
+++ frontend/challedu-app/src/components/RouteForAll.js
@@ -1,17 +1,28 @@
 import React ,{useContext} from "react";
 import {Route, Redirect} from "react-router-dom";
 import UserContext from "../userContext";
-
+import UserProvider from "../UserProvider";
+import { useHistory } from "react-router"
 const RouteForAll = ({component:Component , ...rest }) => {
     const user = useContext(UserContext);
+    const accessTkn = sessionStorage.getItem('access_token');
+    const refreshTkn = sessionStorage.getItem('refresh_token');
+    const history = useHistory();
     return <Route {...rest} render = {(props) =>{
-         return Object.keys(user.user).length && 
-            (   user.user.user.role === "admin" || 
-                user.user.user.role === "user-independent"  || 
-                user.user.user.role === "user-ngo"
-            )? <Component {...props} /> 
-                : <Redirect to ="/login" />
-                
+        if  ( Object.keys(user.tokens).length){  
+            return <Component {...props} /> ; 
+        } else if (accessTkn && refreshTkn) {
+            user.loginTokens({
+                access_token: accessTkn,
+                refresh_token: refreshTkn
+            });
+            let payload = JSON.parse(atob(accessTkn.split(".")[1]));
+            // fetch and save the user
+            UserProvider.getUser(payload.id, accessTkn).then(userData => user.loginUser(userData));
+            history.push(history.location.pathname);
+        } else {
+            return <Redirect to ="/" />;
+        }       
     }}  /> 
 }
 export default RouteForAll
--- frontend/challedu-app/src/components/RouteOnlyForAdmins.js
+++ frontend/challedu-app/src/components/RouteOnlyForAdmins.js
@@ -1,15 +1,33 @@
 import React ,{useContext} from "react";
 import {Route, Redirect} from "react-router-dom";
 import UserContext from "../userContext";
-
-
-
+import UserProvider from "../UserProvider";
+import { useHistory } from "react-router"
 const RouteOnlyForAdmins = ({component:Component , ...rest }) => {
     const user = useContext(UserContext);
-    return <Route {...rest} render = {(props) =>{
-         return Object.keys(user.user).length && user.user.user.role === "admin" ? 
-            <Component {...props} /> : <Redirect to ="/nopermision" />
-                
+    const accessTkn = sessionStorage.getItem('access_token');
+    const refreshTkn = sessionStorage.getItem('refresh_token');
+    const history = useHistory();
+    return <Route {...rest} render = {(props) => {
+        if (Object.keys(user.tokens).length) {
+            let payload = JSON.parse(atob(user.tokens.access_token.split(".")[1]));
+            if (payload.role === "admin"){
+                return <Component {...props} />
+            } else {
+                return <Redirect to ="/nopermision" />;
+            } 
+        } else if (accessTkn && refreshTkn) {
+            user.loginTokens({
+                access_token: accessTkn,
+                refresh_token: refreshTkn
+            });
+            let payload = JSON.parse(atob(accessTkn.split(".")[1]));
+            // fetch and save the user
+            UserProvider.getUser(payload.id, accessTkn).then(userData => user.loginUser(userData))
+            history.push(history.location.pathname);
+        } else {
+            return <Redirect to ="/nopermision" />
+        }     
     }}  /> 
 }
 export default RouteOnlyForAdmins
